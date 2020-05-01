83ff8ebf03fd86423f8d44a17e83935593c32822
Samuel Senior <s.m.senior@soton.ac.uk>
2020-05-01 13:59:18 +0100

    Fixed outline of bell remaining when stroke changes

--- src/bell.py
+++ src/bell.py
@@ -15,15 +15,13 @@ class Bell(Font):
         bell = pygame.image.load(bellImageFile)
         self.handstrokeBell = pygame.transform.scale(bell, (self.width, self.height))
         self.handstrokeBellBlank = self.handstrokeBell.copy()
-        self.fill(self.handstrokeBellBlank, pygame.Color(*self.backgroundColour))
+        self.handstrokeBellBlank.fill(self.backgroundColour)
         self.backstrokeBell = pygame.transform.scale(bell, (self.width, self.height))
         self.backstrokeBell = pygame.transform.rotate(self.backstrokeBell, -90.0)
         self.backstrokeBell = pygame.transform.flip(self.backstrokeBell, True, False)
         self.backstrokeBellBlank = self.backstrokeBell.copy()
-        self.fill(self.backstrokeBellBlank, pygame.Color(*self.backgroundColour))
+        self.backstrokeBellBlank.fill(self.backgroundColour)
 
-        self.bellNumber = bellNumber
-        self.bellNumberText = self.FONT.render(str(self.bellNumber), True, (0, 0, 0))
 
         self.x = location[0]
         self.y = location[1]
@@ -31,27 +29,28 @@ class Bell(Font):
         self.textX = textLocation[0]
         self.textY = textLocation[1]
 
+        self.bellNumber = bellNumber
+        self.bellNumberText = self.FONT.render(str(self.bellNumber), True, (0, 0, 0))
+        self.bellNumberTextBlank = pygame.Rect(self.textX, self.textY, self.bellNumberText.get_width(), self.bellNumberText.get_height())
+
         self.key = key
 
     def fill(self, surface, colour):
-        w, h = surface.get_size()
-        for x in range(w):
-            for y in range(h):
-                a = surface.get_at((x, y))[3]
-                surface.set_at((x, y), pygame.Color(colour[0], colour[1], colour[2], a))
+        surface.fill(colour)
 
     def setKey(self, key):
         self.key = key
 
-    def draw(self, win, renderNumber=False):
-        if renderNumber:
-            win.blit(self.bellNumberText, (self.textX, self.textY))
+    def draw(self, win, renderNumber=True):
         if self.stroke == 'H':
             win.blit(self.backstrokeBellBlank, (self.x, self.y))
-            return win.blit(self.handstrokeBell, (self.x, self.y))
+            win.blit(self.handstrokeBell, (self.x, self.y))
         else:
             win.blit(self.handstrokeBellBlank, (self.x, self.y))
-            return win.blit(self.backstrokeBell, (self.x, self.y))
+            win.blit(self.backstrokeBell, (self.x, self.y))
+        if renderNumber:
+            pygame.draw.rect(win, self.backgroundColour, self.bellNumberTextBlank, 0)
+            win.blit(self.bellNumberText, (self.textX, self.textY))
 
     def handle_event(self, send):
         send("R:" + self.stroke + str(self.bellNumber))
