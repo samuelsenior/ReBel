850335546528640407fc273b6b31dfb888cb6187
Sam W <sw104@users.noreply.github.com>
2020-05-15 13:15:08 +0100

    Corrected font case.

--- src/font.py
+++ src/font.py
@@ -6,9 +6,9 @@ class Font(object):
     def __init__(self, directory):
         self.directory = directory
         pygame.font.init()
-        self.FONT = pygame.font.Font(os.path.join(self.directory, "freesansbold.ttf"), 24)
-        self.largeFont = pygame.font.Font(os.path.join(self.directory, "freesansbold.ttf"), 28)
-        self.mediumFont = pygame.font.Font(os.path.join(self.directory, "freesansbold.ttf"), 24)
-        self.smallFont = pygame.font.Font(os.path.join(self.directory, "freesansbold.ttf"), 20)
-        self.tinyFont = pygame.font.Font(os.path.join(self.directory, "freesansbold.ttf"), 16)
-        self.menu_font = pygame.font.Font(os.path.join(self.directory, "freesansbold.ttf"), 28)
+        self.FONT = pygame.font.Font(os.path.join(self.directory, "FreeSansBold.ttf"), 24)
+        self.largeFont = pygame.font.Font(os.path.join(self.directory, "FreeSansBold.ttf"), 28)
+        self.mediumFont = pygame.font.Font(os.path.join(self.directory, "FreeSansBold.ttf"), 24)
+        self.smallFont = pygame.font.Font(os.path.join(self.directory, "FreeSansBold.ttf"), 20)
+        self.tinyFont = pygame.font.Font(os.path.join(self.directory, "FreeSansBold.ttf"), 16)
+        self.menu_font = pygame.font.Font(os.path.join(self.directory, "FreeSansBold.ttf"), 28)
