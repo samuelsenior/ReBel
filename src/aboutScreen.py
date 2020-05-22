f9b4bfda080f33954c98a0e3faeed937ae08cb1b
Samuel Senior <s.m.senior@soton.ac.uk>
2020-05-22 12:02:31 +0100

    Added code commenting+documentation

--- src/aboutScreen.py
+++ src/aboutScreen.py
@@ -4,28 +4,59 @@ from button import Button
 from textBox import TextBox
 
 class AboutScreen:
+    '''
+    The AboutScreen object displays an About screen with About information.
+    It contains the needed text, the position and dimenions of the about screen,
+    as well as a background fade for the previous screen and a background for
+    the About screen.
 
+    Parameters
+    ----------
+    win : pygame.display
+        The pygame display window.
+
+    font : Font
+        A Font object instance.
+
+    frameRate : int
+        The frame rate at which the pygame display is updated.
+
+    version : string
+        The version of ReBel.
+    '''
     def __init__(self, win, font, frameRate, version):
         self.win = win
         self.font = font
         self.frameRate = frameRate
         self.version = version
 
+        # Set the width and the height of the About screen to 80% of the main window.
         self.width = int(self.win.get_width() * 0.8)
         self.height = int(self.win.get_height() * 0.8)
 
+        # In the AboutScreen frame of reference x and y would both be 0 though their
+        # positions are needed in terms of the main screen reference frame. Therefore
+        # calculate them in terms of main screen reference frame.
         self.x = (self.win.get_width() - self.width) / 2.0
         self.y = (self.win.get_height() - self.height) / 2.0
 
+        # Generate the background fade so that the previous screen will be faded to
+        # show the focus is no longer on it. Fade done via a white surface with an
+        # alpha channel.
         self.backgroundFade = pygame.Surface((self.win.get_width(), self.win.get_height()), pygame.SRCALPHA)
         self.backgroundFade.fill((255, 255, 255, 156))
 
+        # Generate the background of the AboutScreen.
         self.aboutBackground = pygame.Rect(0 + (self.win.get_width() - self.width) / 2.0, 0 + (self.win.get_height() - self.height) / 2.0, self.width, self.height)
 
+        # Generate the buttons present.
         self.button_back = Button("Back", (self.x + 20, self.height + self.y - 20), font=self.font)
         self.button_back.rect.y -= self.button_back.rect.h
         self.buttons = [self.button_back]
 
+        # Generate the text of the AboutScreen. Each text box has word wrapping and
+        # takes the height and y-position of the previous text box, plus an extra
+        # y-spacing of 10, to give its y-position.
         self.aboutTitleText = TextBox('About:',
                                        (self.x+20, self.y+20), width=self.width-40, backgroundColour=(150, 150, 150), font=self.font, fontSize='large')
         self.aboutText_1 = TextBox("ReBel version {}".format(self.version[1:]),
@@ -40,24 +71,52 @@ class AboutScreen:
         self.aboutText = [self.aboutTitleText, self.aboutText_1, self.aboutText_2, self.aboutText_3, self.aboutText_4]
 
     def drawBackground(self):
+        '''
+        Draws the background of the AboutScreen with a border line drawn around
+        it too.
+        '''
         pygame.draw.rect(self.win, (170, 170, 170), self.aboutBackground, 0)
         pygame.draw.rect(self.win, (100, 100, 100), self.aboutBackground, 2)
 
     def display(self, win, source):
+        '''
+        Displays the AboutScreen.
+
+        Parameters
+        ----------
+        win : pygame.display
+            The pygame display window.
+
+        source : string
+            The name of the screen type from where the user is coming from.
+
+        '''
         self.win = win
 
+        # Set the width and the height of the About screen to 80% of the main window.
         self.width = int(self.win.get_width() * 0.8)
         self.height = int(self.win.get_height() * 0.8)
 
+        # Generate the background fade so that the previous screen will be faded to
+        # show the focus is no longer on it. Fade done via a white surface with an
+        # alpha channel.
         self.backgroundFade = pygame.Surface((self.win.get_width(), self.win.get_height()), pygame.SRCALPHA)
         self.backgroundFade.fill((255, 255, 255, 156))
 
+        # Generate the background of the AboutScreen.
         self.aboutBackground = pygame.Rect(0 + (self.win.get_width() - self.width) / 2.0, 0 + (self.win.get_height() - self.height) / 2.0, self.width, self.height)
 
-
+        # In the AboutScreen frame of reference x and y would both be 0 though their
+        # positions are needed in terms of the main screen reference frame. Therefore
+        # calculate them in terms of main screen reference frame.
         self.x = (self.win.get_width() - self.width) / 2.0
         self.y = (self.win.get_height() - self.height) / 2.0
 
+        # The width of the main screen, and therefore the AboutScreen, could have
+        # changed since the text generation. This means that the text needs to be
+        # regenerated to fill the new width. Due to word wrapping this can change
+        # the heights of each text box and so their y-positions need to be re-
+        # calculated.
         for i, aboutText in enumerate(self.aboutText):
             aboutText.rect.x = self.x + 20
             if i == 0:
@@ -65,38 +124,57 @@ class AboutScreen:
             else:
                 startingY = self.aboutText[i-1].y + self.aboutText[i-1].h+10
             aboutText.generateFormattedText(startingY=startingY, width=self.width-40)
+        # The width of the main screen could have changed, meaning the AboutScreen
+        # width and x could have changed. Therefore need to update the button -
+        # postions
         self.button_back.rect.x = self.x + 20
 
+        # Apply the bakground fade to the previous screen the user is coming from
+        # and draw the background of the AboutScreen.
         self.win.blit(self.backgroundFade, (0, 0))
-
         self.drawBackground()
 
+        # Update the button to not being hovered over by the mouse and draw them.
         for button in self.buttons:
             if button.updated:
                 button.hovered = False
                 button.draw(self.win)
 
+        # Draw the text to the screen.
         for txt in self.aboutText:
             txt.draw(self.win)
 
+        # Update the screen to display the updated draws.
         pygame.display.flip()
         
+        # Pygame clock for setting frame rate.
         clock = pygame.time.Clock()
         
+        # Main display loop.
         run_about = True
         while run_about:
-
+            # Get the event.
             for event in pygame.event.get():
+                # If the event is a quit event then quit the display loop and
+                # return 'quit' so that the main program loop can do a clean
+                # quit.
                 if event.type == pygame.QUIT:
                     run_about = False
                     return 'quit'
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
+                    # If the event is a mouse down click and its position is 
+                    # on the back button then return name of the previous
+                    # screen so that the user goes back to that screen.
                     if self.button_back.rect.collidepoint(event.pos):
                         run_about = False
                         return source
 
                 for button in self.buttons:
+                    # If the mouse is hovering over a button then update the
+                    # button to have the hovered text colour. If the button
+                    # is active and is no longer being hovered over then
+                    # return the text colour back to its original colour.
                     if button.rect.collidepoint(pygame.mouse.get_pos()):
                         button.hovered = True
                         button.updated = True
@@ -105,8 +183,13 @@ class AboutScreen:
                         button.updated = True
 
             for button in self.buttons:
+                # If any of the buttons have had changes happen to them
+                # then redraw them.
                 if button.updated:
                     button.draw(self.win)
 
+            # Update the screen to display the updated draws.
             pygame.display.flip()
+
+            # Pause the loop until all of the frame rate time has passed.
             clock.tick(self.frameRate)
